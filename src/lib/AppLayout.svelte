39d16c8a10afa06fd357b38ef35b9e1bb20ba445
Alexis LOURS <alexislours@protonmail.com>
2026-04-27 11:11:39 +0200

    feat: add FAQ page

--- src/lib/AppLayout.svelte
+++ src/lib/AppLayout.svelte
@@ -47,6 +47,7 @@
     { href: '/player', label: $_('tab.player'), wip: false },
     { href: '/mii', label: $_('tab.mii'), wip: false },
     { href: '/map', label: $_('tab.map'), wip: false },
+    { href: '/faq', label: $_('tab.faq'), wip: false },
     { href: '/about', label: $_('tab.about'), wip: false },
   ]);
 
