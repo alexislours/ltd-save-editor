853aec6bd2e485b1ae17ad6e15e85c6d16a74dbc
Alexis LOURS <alexislours@protonmail.com>
2026-04-27 22:10:30 +0200

    refactor: simplify wide string size calculation

--- src/lib/sav/codec.ts
+++ src/lib/sav/codec.ts
@@ -127,15 +127,7 @@ export function getString(e: Entry): string {
 
 export function stringEncodedSize(t: DataType, s: string): number {
   if (NARROW_STRINGS.has(t)) return new TextEncoder().encode(s).byteLength;
-  if (WIDE_STRINGS.has(t)) {
-    let units = 0;
-    for (let i = 0; i < s.length; i++) {
-      const c = s.charCodeAt(i);
-      void c;
-      units++;
-    }
-    return units * 2;
-  }
+  if (WIDE_STRINGS.has(t)) return s.length * 2;
   throw new Error(`Not a string type: ${t}`);
 }
 
